88ef0a385e56098b538b95daedfa95a2b6075324
davexsullivan <62624942+davexsullivan@users.noreply.github.com>
2020-04-15 13:28:52 -0400

    main files

--- Dave_Sullivan_R_work_climate.R
+++ Dave_Sullivan_R_work_climate.R
@@ -4,6 +4,8 @@ install.packages('ggplot2')
 install.packages("dplyr")
 install.packages("corrplot")
 install.packages("ggpubr")
+install.packages("readxl")
+
 
 library(weathercan)
 library(ggplot2)
@@ -11,37 +13,235 @@ library(sp)
 library(dplyr)
 library(corrplot)
 library(ggpubr)
+library(readxl)
 
 #Checking to ensure 64-bit version of R in use, to prevent memory errors
 sessionInfo() 
 
 #getting data from 150km radius of Toronto
-toronto_100_day_list<-  stations_search(coords=c(43.6,-79.3), dist=150, interval='day', starts_latest=1984, ends_earliest = 2017)
-toronto_100_month_list<-  stations_search(coords=c(43.6,-79.3), dist=150, interval='month', starts_latest=1984, ends_earliest = 2017)
+attach(stations)
 
-toronto_100_day_data<-weather_dl(station_ids= c(toronto_100_day_list$station_id), start='1980-01-01', interval='day', string_as = NULL, verbose=TRUE)
-toronto_100_month_data<-weather_dl(station_ids= c(toronto_100_month_list$station_id), start='1980-01-01', interval='month', string_as = NULL, verbose=TRUE)
+toronto_200_day_list<-  stations_search(coords=c(43.6,-79.3), dist=200, 
+                                        interval='day', starts_latest=1980, ends_earliest = 2017)
+toronto_200_data<-weather_dl(station_ids= c(toronto_200_day_list$station_id), start='1980-01-01', 
+                             interval='day', string_as = NULL, verbose=TRUE)
+write.csv(toronto_200_data, "C:/GIT/climate/data/toronto_200_data.csv")
 
-write.csv(toronto_100_day_data, "C:/data_analytics/climate/toronto_100_day_data.csv")
-write.csv(toronto_100_month_data, "C:/data_analytics/climate/toronto_100_month_data.csv")
+vancouver_200_day_list<-  stations_search(coords=c(49.4,-123.1), dist=200, interval='day',
+    starts_latest=1980, ends_earliest = 2017)
+vancouver_200_data<-weather_dl(station_ids= c(vancouver_200_day_list$station_id), 
+                               start='1980-01-01', interval='day', string_as = NULL, verbose=TRUE)
+write.csv(vancouver_200_data, "C:/GIT/climate/data/vancouver_200_data.csv")
+
 
 #Getting all stations data with appropriate date range
-all_stations_day_list<-filter(stations,  stations$start<=1950, stations$end>=2018, interval=='day' )
-all_stations_month_list<-filter(stations,  stations$start<=1980, stations$end>=2018, interval=='month' )
+all_stations_day_list<-filter(stations,  start<=1975, end>=2019, interval=='day' )
+all_stations_month_list<-filter(stations,  start<=1980, end>=2016, interval=='month' )
 
-all_stations_day_data<-weather_dl(station_ids= c(all_stations_day_list$station_id), start='1980-01-01', interval='day', string_as = NULL, verbose=TRUE)
-all_stations_month_data<-weather_dl(station_ids= c(all_stations_month_list$station_id), start='1980-01-01', interval='month', string_as = NULL, verbose=TRUE)
+#Attempting to download all at once caused crashes (~4mil rows)
+#Split into equal smaller groups
+quantile(all_stations_day_list$station_id)
 
-write.csv(all_stations_day_data, "C:/data_analytics/climate/all_stations_day_data.csv")
-write.csv(all_stations_month_data, "C:/data_analytics/climate/all_stations_month_data.csv")
+all_stations_day_list1<-filter(stations, station_id<1181,  start<=1975, end>=2019, interval=='day' )
+all_stations_day_list2<-filter(stations, station_id>=1181, station_id<3888,
+                               start<=1975, end>=2019, interval=='day' )
+all_stations_day_list3<-filter(stations, station_id>=3888, station_id<5423,
+                               start<=1975, end>=2019, interval=='day' )
+all_stations_day_list4<-filter(stations, station_id>=5423, start<=1975, end>=2019, interval=='day' )
+
+all_stations_day_data1<-weather_dl(station_ids= c(all_stations_day_list1$station_id), 
+                                   start='1980-01-01', interval='day', string_as = NULL, verbose=TRUE)
+all_stations_day_data2<-weather_dl(station_ids= c(all_stations_day_list2$station_id), 
+                                   start='1980-01-01', interval='day', string_as = NULL, verbose=TRUE)
+all_stations_day_data3<-weather_dl(station_ids= c(all_stations_day_list3$station_id), 
+                                   start='1980-01-01', interval='day', string_as = NULL, verbose=TRUE)
+all_stations_day_data4<-weather_dl(station_ids= c(all_stations_day_list4$station_id), 
+                                   start='1980-01-01', interval='day', string_as = NULL, verbose=TRUE)
+
+all_stations_data<-rbind(all_stations_day_data1,all_stations_day_data2,all_stations_day_data3,all_stations_day_data4)
+write.csv(all_stations_data, "C:/GIT/climate/data/all_stations_data.csv")
+
+#Create unique list of stations for use as an index
+list_all_stations<-rbind(toronto_200_day_list[,c(1,2,3,4,5,6,7,8,9,10,11,12,13,14)],
+      vancouver_200_day_list[,c(1,2,3,4,5,6,7,8,9,10,11,12,13,14)],all_stations_day_list )
+which(duplicated(list_all_stations$station_id))
+list_all_stations<-list_all_stations[!duplicated(list_all_stations$station_id), ]
+write.csv(list_all_stations, "C:/GIT/climate/data/station_index.csv")
+
+#Export data to SQL for cleaning and aggregation
+#This is where climate_sql_work.sql  is run
+
+#Import the finalized data from sql:
+
+
+#Importing all the aggregated data
+all_stations_annual <- read.csv("C:/GIT/climate/data/all_stations_annual.csv", header = TRUE, sep = ",")
+all_stations_summer <- read.csv("C:/GIT/climate/data/all_stations_summer.csv", header = TRUE, sep = ",")
+toronto_daily <- read.csv("C:/GIT/climate/data/toronto_daily.csv", header = TRUE, sep = ",")
+toronto_annual <- read.csv("C:/GIT/climate/data/toronto_annual.csv", header = TRUE, sep = ",")
+toronto_summer <- read.csv("C:/GIT/climate/data/toronto_summer.csv", header = TRUE, sep = ",")
+vancouver_annual <- read.csv("C:/GIT/climate/data/vancouver_annual.csv", header = TRUE, sep = ",")
+vancouver_summer <- read.csv("C:/GIT/climate/data/vancouver_summer.csv", header = TRUE, sep = ",")
+
+
+#Selecting the larger latitude groups by population, keeping only the measures
+all_47to49 <- subset(all_stations_annual,lat_range=='47to49',c(3,4,5,6,7,8,9))
+all_49to51 <- subset(all_stations_annual,lat_range=='49to51',c(3,4,5,6,7,8,9))
+all_summer_47to49 <- subset(all_stations_summer,lat_range=='47to49',c(3,4,5,6,7,8,9))
+all_summer_49to51 <- subset(all_stations_summer,lat_range=='49to51',c(3,4,5,6,7,8,9))
+
+#Same for the 2 regions
+tor<-toronto_annual[,c(2,3,4,5,6,7,8)]
+tor_summer<-toronto_summer[,c(2,3,4,5,6,7,8)]
+tor_daily<-toronto_summer[,c(2,3,4,5,6,7)]
+
+van<-vancouver_annual[,c(2,3,4,5,6,7,8)]
+van_summer<-vancouver_summer[,c(2,3,4,5,6,7,8)]
+
+#Examine the correlations
+cor(tor)
+cor(tor_summer)
+cor(van)
+cor(van_summer)
+cor(all_47to49)
+cor(all_49to51)
+cor(all_summer_47to49)
+cor(all_summer_49to51)
+
+
+#Annual data
+head(tor,5)
+summary(lm(tor$daily_max_temp_avg ~ tor$co2_ppm))
+summary(lm(tor$daily_max_temp_avg ~ tor$avg_toa_insolation))
+summary(lm(tor$daily_max_temp_avg ~ tor$avg_sky_ins))
+
+
+summary(lm(van$daily_max_temp_avg ~ van$co2_ppm))
+summary(lm(van$daily_max_temp_avg ~ van$avg_toa_insolation))
+summary(lm(van$daily_max_temp_avg ~ van$avg_sky_ins))
+
+summary(lm(all_47to49$daily_max_temp_avg ~ all_47to49$co2_ppm))
+summary(lm(all_47to49$daily_max_temp_avg ~ all_47to49$avg_toa_insolation))
+
+summary(lm(all_49to51$daily_max_temp_avg ~ all_49to51$co2_ppm))
+summary(lm(all_49to51$daily_max_temp_avg ~ all_49to51$avg_toa_insolation))
+
+#Summer data
+head(tor_summer,5)
+summary(lm(tor_summer$summer_max_temp_avg ~ tor_summer$co2_ppm))
+summary(lm(tor_summer$summer_max_temp_avg ~ tor_summer$summer_avg_toa_insolation))
+summary(lm(tor_summer$summer_max_temp_avg ~ tor_summer$summer_avg_sky_insolation))
+
+summary(lm(van_summer$summer_max_temp_avg ~ van_summer$co2_ppm))
+summary(lm(van_summer$summer_max_temp_avg ~ van_summer$summer_avg_toa_insolation))
+summary(lm(van_summer$summer_max_temp_avg ~ van_summer$summer_avg_sky_insolation))
+
+summary(lm(all_summer_47to49$summer_max_temp_avg ~ all_summer_47to49$co2_ppm))
+summary(lm(all_summer_47to49$summer_max_temp_avg ~ all_summer_47to49$summer_avg_toa_insolation))
+
+summary(lm(all_summer_49to51$summer_max_temp_avg ~ all_summer_49to51$co2_ppm))
+summary(lm(all_summer_49to51$summer_max_temp_avg ~ all_summer_49to51$summer_avg_toa_insolation))
+
+corrplot(cor(all_47to49))
+corrplot(cor(all_49to51))
+corrplot(cor(all_summer_47to49))
+corrplot(cor(all_summer_49to51))
+corrplot(cor(tor))
+corrplot(cor(tor_summer))
+corrplot(cor(tor_daily))
+corrplot(cor(van))
+corrplot(cor(van_summer))
 
 
 
-all_stations_final <- read.csv("C:/data_analytics/climate/all_stations_final.csv", header = TRUE, sep = ",")
-toronto_stations_final <- read.csv("C:/data_analytics/climate/toronto_stations_final.csv", header = TRUE, sep = ",")
-toronto<-toronto_stations_final[,]
+#Focus on one set
+head(all_summer_49to51,5)
+attach(all_summer_49to51)
 
-ggscatter(my_data, x = "mpg", y = "wt", 
-          add = "reg.line", conf.int = TRUE, 
-          cor.coef = TRUE, cor.method = "pearson",
-          xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+summary(lm(all_summer_49to51$summer_mean_temp_avg ~ all_summer_49to51$co2_ppm))
+summary(lm(summer_mean_temp_avg ~ summer_avg_sky_insolation))
+summary(lm(summer_mean_temp_avg ~ summer_avg_toa_insolation))
+
+
+
+
+
+corrplot(cor(all_summer_47to49), type="upper", , 
+          sig.level = 0.01,tl.col="black", tl.srt=45,title="
+         Latitudes47to49 summer months" )
+
+corrplot(cor(all_summer_49to51), type="upper", , 
+         sig.level = 0.01,tl.col="black", tl.srt=45,title="
+         Latitudes49to51 summer months" )
+
+corrplot(cor(tor_summer), type="upper", , 
+         sig.level = 0.01,tl.col="black", tl.srt=45,title="
+         Toronto summer months" )
+
+corrplot(cor(van_summer), type="upper", , 
+         sig.level = 0.01,tl.col="black", tl.srt=45,title="
+         Vancouver summer months" )
+
+
+scatter.smooth(x=summer_mean_temp_avg, y=co2_ppm, main="Temp ~ CO2")  # scatterplot
+scatter.smooth(x=summer_mean_temp_avg, y=summer_avg_toa_insolation, main="Temp ~ Solar")  # scatterplot
+
+
+ggscatter(all_summer_49to51, x = "summer_mean_temp_avg", y = "summer_avg_sky_insolation", 
+          add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Avg Summer Temperature, Celcius", ylab = "Ground Level Solar")
+
+
+ggscatter(all_summer_49to51, x = "summer_mean_temp_avg", y = "summer_avg_toa_insolation", 
+          add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Avg Summer Temperature, Celcius", ylab = "Solar Activity")
+
+
+ggscatter(all_summer_49to51, x = "summer_mean_temp_avg", y = "co2_ppm", 
+          add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Avg Summer Temperature, Celcius", ylab = "Carbon Dioxide")
+
+
+shapiro.test(summer_mean_temp_avg) #p-value = 0.7702
+shapiro.test(co2_ppm) #p-value = 0.1149
+shapiro.test(summer_avg_toa_insolation) #p-value = 0.0009933
+
+
+#Now Just Toronto
+head(tor,5)
+attach(tor_summer)
+
+summary(lm(summer_mean_temp_avg ~ co2_ppm))
+summary(lm(summer_mean_temp_avg ~ avg_sky_ins))
+summary(lm(summer_mean_temp_avg ~ avg_toa_insolation))
+
+summary(lm(daily_max_temp_avg ~ co2_ppm))
+summary(lm(daily_max_temp_avg ~ avg_sky_ins))
+summary(lm(daily_max_temp_avg ~ avg_toa_insolation))
+
+
+
+scatter.smooth(x=daily_mean_temp_avg, y=co2_ppm, main="Temp ~ CO2")  # scatterplot
+scatter.smooth(x=daily_mean_temp_avg, y=avg_toa_insolation, main="Temp ~ Solar")  # scatterplot
+
+
+ggscatter(tor, x = "daily_mean_temp_avg", y = "avg_sky_ins", 
+          add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Avg Summer Temperature, Celcius", ylab = "Ground Level Solar")
+
+
+ggscatter(tor, x = "daily_mean_temp_avg", y = "avg_toa_insolation", 
+          add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Avg Summer Temperature, Celcius", ylab = "Solar Activity")
+
+
+ggscatter(tor, x = "daily_mean_temp_avg", y = "co2_ppm", 
+          add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Avg Summer Temperature, Celcius", ylab = "Carbon Dioxide")
+
+
+shapiro.test(daily_mean_temp_avg) #p-value = 0.07504
+shapiro.test(co2_ppm) #p-value = 0.1149
+shapiro.test(avg_toa_insolation) #p-value = 5.342e-07
+
+
--- Dave_Sullivan_climate_sql_work.sql
+++ Dave_Sullivan_climate_sql_work.sql
@@ -1,4 +1,4 @@
-create table toronto_100_day (
+create table toronto_200 (
 num int (10), 
 station_name varchar(255),
 station_id int(10),
@@ -39,7 +39,7 @@ total_snow decimal(10,2),
 total_snow_flag varchar(22)
 );
 
-create table toronto_100_month (
+create table vancouver_200 (
 num int (10), 
 station_name varchar(255),
 station_id int(10),
@@ -80,48 +80,7 @@ total_snow decimal(10,2),
 total_snow_flag varchar(22)
 );
 
-create table all_stations_month (
-num int (10), 
-station_name varchar(255),
-station_id int(10),
-station_operator varchar(22),
-prov varchar(5),
-lat decimal(8,2),
-lon decimal(8,2),
-elev int(8),
-climate_id int(22),
-WMO_id int(22),
-TC_id int(22),
-date date,
-year int(5),
-month int(5),
-day int(5),
-qual varchar(255),
-cool_deg_days decimal(10,2),
-cool_deg_days_flag varchar(22),
-dir_max_gust varchar(22),
-dir_max_gust_flag varchar(22),
-heat_deg_days decimal(10,2),
-heat_deg_days_flag varchar(22),
-max_temp decimal(10,2),
-max_temp_flag varchar(22),
-mean_temp decimal(10,2),
-mean_temp_flag varchar(22),
-min_temp decimal(10,2),
-min_temp_flag varchar(22),
-snow_grnd decimal(10,2),
-snow_grnd_flag varchar(22),
-spd_max_gust decimal(10,2),
-spd_max_gust_flag varchar(22),
-total_precip decimal(10,2),
-total_precip_flag varchar(22),
-total_rain decimal(10,2),
-total_rain_flag varchar(22),
-total_snow decimal(10,2),
-total_snow_flag varchar(22)
-);
-
-create table all_stations_day (
+create table all_stations (
 num int (10), 
 station_name varchar(255),
 station_id int(10),
@@ -163,27 +122,14 @@ total_snow_flag varchar(22)
 );
 
 
-load data local infile 'C:/data_analytics/climate/toronto_100_day_data.csv' into table toronto_100_day fields 
+load data local infile 'C:/GIT/climate/data/toronto_200_data.csv' into table toronto_200 fields 
 terminated by ',' OPTIONALLY ENCLOSED by '"' lines terminated by '\r' ignore 1 lines;
 
-load data local infile 'C:/data_analytics/climate/toronto_100_month_data.csv' into table toronto_100_month fields 
+load data local infile 'C:/GIT/climate/data/vancouver_200_data.csv' into table vancouver_200 fields 
 terminated by ',' OPTIONALLY ENCLOSED by '"' lines terminated by '\r' ignore 1 lines;
 
-load data local infile 'C:/data_analytics/climate/lat40_day_data.csv' into table all_stations_day fields 
+load data local infile 'C:/GIT/climate/data/all_stations_data.csv' into table all_stations fields 
 terminated by ',' OPTIONALLY ENCLOSED by '"' lines terminated by '\r' ignore 1 lines;
-load data local infile 'C:/data_analytics/climate/lat50_day_data.csv' into table all_stations_day fields 
-terminated by ',' OPTIONALLY ENCLOSED by '"' lines terminated by '\r' ignore 1 lines;
-load data local infile 'C:/data_analytics/climate/lat60_day_data.csv' into table all_stations_day fields 
-terminated by ',' OPTIONALLY ENCLOSED by '"' lines terminated by '\r' ignore 1 lines;
-
-load data local infile 'C:/data_analytics/climate/lat40_month_data.csv' into table all_stations_month fields 
-terminated by ',' OPTIONALLY ENCLOSED by '"' lines terminated by '\r' ignore 1 lines;
-load data local infile 'C:/data_analytics/climate/lat50_month_data.csv' into table all_stations_month fields 
-terminated by ',' OPTIONALLY ENCLOSED by '"' lines terminated by '\r' ignore 1 lines;
-load data local infile 'C:/data_analytics/climate/lat60_month_data.csv' into table all_stations_month fields 
-terminated by ',' OPTIONALLY ENCLOSED by '"' lines terminated by '\r' ignore 1 lines;
-
-
 
 
 create table co2_levels (
@@ -192,56 +138,240 @@ co2_ppm decimal(10,2),
 uncertainty decimal(10,2)
 );
 
-create table solar_levels_toronto (
-year int(5), 
-Jan decimal(10,2),
-Feb decimal(10,2),
-Mar decimal(10,2),
-Apr decimal(10,2),
-May decimal(10,2),
-Jun decimal(10,2),
-Jul decimal(10,2),
-Aug decimal(10,2),
-Sep decimal(10,2),
-Oct decimal(10,2),
-Nov decimal(10,2),
-`Dec` decimal(10,2), 
-annual decimal(10,2)
+create table solar_rad_global (
+`date` date, 
+Radiation decimal(10,2), 
+STD decimal(10,2)
 );
 
-load data local infile 'C:/data_analytics/climate/CO2.csv' into table co2_levels fields 
+create table toronto_solar (
+lat decimal(10,2), 
+lon decimal(10,2), 
+year int(5), 
+doy int(5), 
+therm_flux decimal(10,2),
+sky_insolation decimal(10,2),
+toa_insolation decimal(10,2)
+);
+
+create table vancouver_solar (
+lat decimal(10,2), 
+lon decimal(10,2), 
+year int(5), 
+doy int(5), 
+therm_flux decimal(10,2),
+sky_insolation decimal(10,2),
+toa_insolation decimal(10,2)
+);
+
+load data local infile 'C:/data_analytics/climate/solar_daily_toronto.csv' into table toronto_solar fields 
 terminated by ','  lines terminated by '\n' ignore 1 lines;
-load data local infile 'C:/data_analytics/climate/solar_to.csv' into table solar_levels_toronto fields 
+load data local infile 'C:/GIT/climate/data/solar_daily_vancouver.csv' into table vancouver_solar fields 
 terminated by ','  lines terminated by '\n' ignore 1 lines;
 
+alter table toronto_solar add column date date, add index(date);
+alter table vancouver_solar add column date date, add index(date);
+
+load data local infile 'C:/GIT/climate/data/CO2_annual.csv' into table co2_levels fields 
+terminated by ','  lines terminated by '\n' ignore 1 lines;
+load data local infile 'C:/GIT/climate/data/satellite_solar_data.csv' into table solar_rad_global fields 
+terminated by ',' OPTIONALLY ENCLOSED by '"' lines terminated by '\r' ignore 1 lines;
+
+alter table toronto_solar  add index(date);
+alter table vancouver_solar  add index(date);
+
+update toronto_solar set date=concat(makedate(year, doy));
+update vancouver_solar set date=concat(makedate(year, doy));
 
 
 
 
-#Main populated latitude range data
-select year(date), avg(lat) as avg_lat, 
-count(distinct station_id) as stations_used, avg(if(max_temp!=0, max_temp, null)) as avg_max_temp, 
-avg(if(mean_temp!=0, max_temp, null)) as avg_mean_temp , max(max_temp) as max_temp, 
-avg(if(max_temp!=0 and month in (7,8), max_temp, null)) as avg_max_summer_temp,
-max(if(max_temp!=0 and month in (7,8), max_temp, null)) as max_max_summer_temp, co2_ppm
-from all_stations_month a
-left join co2_levels c on year(a.date)=c.year
-where lat>43 and lat<50
+
+
+
+
+
+
+
+alter table all_stations add index(date), add index(station_id);
+alter table toronto_200 add index(date), add index(station_id);
+alter table vancouver_200 add index(date), add index(station_id);
+
+#Figure out the latitude range groupings
+select round(lat, 0) as latt, count(distinct station_id) as stations from all_stations group by latt order by latt;
+
+select case when lat<46 then 'under46' when lat>=46 and lat<47 then '46to47' when lat>=47 and lat<49 then '47to49'
+when lat>=49 and lat<51 then '49to51'when lat>=51 and lat<54 then '51to54' else '54andup' end as lat_range,
+ count(distinct station_id) as stations_used
+ from all_stations as2 group by lat_range;
+ 
+#Main populated latitude range aggregated annual
+create table all_stations_annual
+select year(a.date) as year, avg(a.lat) as avg_lat, 
+case when a.lat<46 then 'under46' when a.lat>=46 and a.lat<47 then '46to47' when a.lat>=47 and a.lat<49 then '47to49'
+when a.lat>=49 and a.lat<51 then '49to51'when a.lat>=51 and a.lat<54 then '51to54' else '54andup' end as lat_range,
+count(distinct station_id) as stations_used, avg(if(max_temp!=0, max_temp, null)) as daily_max_temp_avg, 
+avg(if(mean_temp!=0, max_temp, null)) as daily_mean_temp_avg , max(max_temp) as max_annual_temp, 
+avg(if(max_temp!=0 and month in (7,8), max_temp, null)) as summer_max_temp_avg,
+avg(if(mean_temp!=0 and month in (7,8), mean_temp, null)) as summer_mean_temp_avg,
+max(if(max_temp!=0 and month in (7,8), max_temp, null)) as summer_max_temp
+from all_stations  a
+where a.date is not null
+group by lat_range,year(date) 
+order by  lat_range,
+year(a.date);
+
+
+#Toronto stations aggregated daily
+create table toronto_daily
+select a.date, avg(a.lat) as avg_lat, 
+count(distinct station_id) as stations_used, avg(if(max_temp!=0, max_temp, null)) as daily_max_temp_avg, 
+avg(if(mean_temp!=0, max_temp, null)) as daily_mean_temp_avg , max(max_temp) as max_daily_temp 
+from toronto_200 a
+where date is not null
+group by date 
+order by a.date;
+
+#Toronto stations aggregated annual
+create table toronto_annual
+select year(a.date) as year, avg(a.lat) as avg_lat, 
+count(distinct station_id) as stations_used, avg(if(max_temp!=0, max_temp, null)) as daily_max_temp_avg, 
+avg(if(mean_temp!=0, max_temp, null)) as daily_mean_temp_avg , max(max_temp) as max_annual_temp, 
+avg(if(max_temp!=0 and month in (7,8), max_temp, null)) as summer_max_temp_avg,
+avg(if(mean_temp!=0 and month in (7,8), mean_temp, null)) as summer_mean_temp_avg,
+max(if(max_temp!=0 and month in (7,8), max_temp, null)) as summer_max_temp
+from toronto_200 a
+where date is not null
 group by year(date) 
-order by  year(date);
+order by year(a.date);
+
+#Vancouver stations aggregated daily
+create table vancouver_daily
+select a.date, avg(a.lat) as avg_lat, 
+count(distinct station_id) as stations_used, avg(if(max_temp!=0, max_temp, null)) as daily_max_temp_avg, 
+avg(if(mean_temp!=0, max_temp, null)) as daily_mean_temp_avg , max(max_temp) as max_daily_temp 
+from vancouver_200 a
+where date is not null
+group by date 
+order by a.date;
+
+#Vancouver stations aggregated annual
+create table vancouver_annual
+select year(a.date) as year, avg(a.lat) as avg_lat, 
+count(distinct station_id) as stations_used, avg(if(max_temp!=0, max_temp, null)) as daily_max_temp_avg, 
+avg(if(mean_temp!=0, max_temp, null)) as daily_mean_temp_avg , max(max_temp) as max_annual_temp, 
+avg(if(max_temp!=0 and month in (7,8), max_temp, null)) as summer_max_temp_avg,
+avg(if(mean_temp!=0 and month in (7,8), mean_temp, null)) as summer_mean_temp_avg,
+max(if(max_temp!=0 and month in (7,8), max_temp, null)) as summer_max_temp
+from vancouver_200  a
+where date is not null
+group by year(date) 
+order by year(a.date);
 
 
-#Toronto data
-select year(date), count(distinct station_id) as stations_used, avg(if(max_temp!=0, max_temp, null)) as avg_max_temp, 
-avg(if(mean_temp!=0, max_temp, null)) as avg_mean_temp , max(max_temp) as max_temp, min(min_temp) as min_temp, 
-avg(if(max_temp!=0 and month in (7,8), max_temp, null)) as avg_max_summer_temp,
-max(if(max_temp!=0 and month in (7,8), max_temp, null)) as max_max_summer_temp.
-co2_ppm, s.annual as annual_solar, (s.Jul+s.Aug)/2 as summer_solar
-from toronto_100_day t 
-left join solar_levels_toronto s on year(t.date)=s.year
-left join co2_levels c on year(t.date)=c.year
-where t.date is not null and year(t.date)>=1980
-group by year(date)
-order by  year(date);
 
-#Export both results to CSV format;  main_set.csv  & toronto_set.csv
+
+#Toronto solar aggreagated annual
+create table toronto_solar_annual
+select year(t.date) as year, 
+avg(t.therm_flux) as avg_therm_flux, 
+avg(if(month(t.date) in (7,8), t.therm_flux, null)) as summer_avg_therm_flux,
+avg(t.sky_insolation) as avg_sky_ins, 
+avg(if(month(t.date) in (7,8), t.sky_insolation, null)) as summer_avg_sky_insolation,
+avg(t.toa_insolation) as avg_toa_insolation, 
+avg(if(month(t.date) in (7,8), t.toa_insolation, null)) as summer_avg_toa_insolation
+from toronto_solar t
+where year(date)>=1984 and year(date)<=2019
+group by year(t.date) 
+order by year(t.date);
+
+
+
+#Vancouver solar aggreagated annual
+create table vancouver_solar_annual
+select year(v.date) as year,
+avg(v.therm_flux) as avg_therm_flux, 
+avg(if(month(v.date) in (7,8), v.therm_flux, null)) as summer_avg_therm_flux,
+avg(v.sky_insolation) as avg_sky_ins, 
+avg(if(month(v.date) in (7,8), v.sky_insolation, null)) as summer_avg_sky_insolation,
+avg(v.toa_insolation) as avg_toa_insolation, 
+avg(if(month(v.date) in (7,8), v.toa_insolation, null)) as summer_avg_toa_insolation
+from vancouver_solar v 
+where year(date)>=1984 and year(date)<=2019
+group by year(v.date) order by year(v.date);
+
+
+
+
+
+#TOronto daily data export
+select td.date, daily_max_temp_avg, daily_mean_temp_avg, max_daily_temp,
+therm_flux, sky_insolation, toa_insolation 
+#INTO OUTFILE 'toronto_daily.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"'  LINES TERMINATED BY '\n' 
+from toronto_daily td 
+ join toronto_solar ts on td.date=ts.date
+group by td.date;
+
+
+#All annual data export
+select lat_range, t.year, daily_max_temp_avg, daily_mean_temp_avg, max_annual_temp, 
+avg_therm_flux, avg_sky_ins, avg_toa_insolation,
+co2_ppm
+INTO OUTFILE 'vancouver_annual.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"'  LINES TERMINATED BY '\n' 
+from all_stations_annual t
+join toronto_solar_annual tsa on t.year=tsa.year
+left join co2_levels c on t.year=c.year
+group by lat_range, t.year;
+
+#All summer data export
+select lat_range, t.year,
+summer_max_temp_avg, summer_mean_temp_avg, summer_max_temp, 
+summer_avg_therm_flux,summer_avg_sky_insolation, summer_avg_toa_insolation,
+co2_ppm
+INTO OUTFILE 'vancouver_annual.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"'  LINES TERMINATED BY '\n' 
+from all_stations_annual t
+join toronto_solar_annual tsa on t.year=tsa.year
+left join co2_levels c on t.year=c.year
+group by lat_range, t.year;
+
+
+
+#Toronto annual data export
+select t.year, daily_max_temp_avg, daily_mean_temp_avg, max_annual_temp, 
+avg_therm_flux, avg_sky_ins, avg_toa_insolation,
+co2_ppm
+INTO OUTFILE 'vancouver_annual.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"'  LINES TERMINATED BY '\n' 
+from toronto_annual t
+join toronto_solar_annual tsa on t.year=tsa.year
+left join co2_levels c on t.year=c.year;
+
+#Toronto summer data export
+select t.year,
+summer_max_temp_avg, summer_mean_temp_avg, summer_max_temp, 
+summer_avg_therm_flux,summer_avg_sky_insolation, summer_avg_toa_insolation,
+co2_ppm
+INTO OUTFILE 'vancouver_annual.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"'  LINES TERMINATED BY '\n' 
+from toronto_annual t
+join toronto_solar_annual tsa on t.year=tsa.year
+left join co2_levels c on t.year=c.year;
+
+#Vancouver annual data export
+select v.year, daily_max_temp_avg, daily_mean_temp_avg, max_annual_temp, 
+avg_therm_flux, avg_sky_ins, avg_toa_insolation,
+co2_ppm
+INTO OUTFILE 'vancouver_annual.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"'  LINES TERMINATED BY '\n' 
+from vancouver_annual v
+join vancouver_solar_annual vsa on v.year=vsa.year
+left join co2_levels c on v.year=c.year;
+
+#Vancouver summer data export
+select v.year,
+summer_max_temp_avg, summer_mean_temp_avg, summer_max_temp, 
+summer_avg_therm_flux,summer_avg_sky_insolation, summer_avg_toa_insolation,
+co2_ppm 
+INTO OUTFILE 'vancouver_summer.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' 
+from vancouver_annual v
+join vancouver_solar_annual vsa on v.year=vsa.year
+left join co2_levels c on v.year=c.year;
+
+
